6a51a47beeff7428e65c59326cead505661978a6
Kyrylo263 <kyryloz2000@gmail.com>
2023-06-10 18:33:32 +0300

    add1

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const celsiy = 30;
+const farengeyt = 50;
+const celsiySecond = (farengeyt - 32) / 1.8;
+const farengeytSecond = celsiy * 1.8 + 32;
+console.log(celsiySecond);
+console.log(farengeytSecond);
+
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = hoursInMonth * 60;
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+let hpOfPlayer = 100;
+let energyOfPlayer = 100;
+console.log(hpOfPlayer - 84);
+console.log(energyOfPlayer - 43);
+
+const sumaPokupky = 4500;
+console.log(sumaPokupky * 0.9);
+
+const chysloWithSwimKoma = 0.5 + 0.7;
+console.log(Math.floor(chysloWithSwimKoma));
+
+const stringFloat = '156.46383dsfsdj32';
+console.log(Number.parseFloat(stringFloat));
+
+const stringInt = '156.46383dsfsdj32';
+console.log(Number.parseInt(stringInt));
+
+const number = 361;
+console.log(Math.sqrt(number));
+
+const numberFull = 3423;
+const stringNumber = '34jkjdfv645';
+console.log(Number.parseInt(stringNumber));
+console.log(numberFull.toString());
